209c02c6a0f3b73425380da4cc5ff2c05d01e744
soumyat97 <37169644+soumyat97@users.noreply.github.com>
2020-10-27 11:21:27 +0530

    Add files via upload

--- tests/unit/Dashboard.spec.js
+++ tests/unit/Dashboard.spec.js
@@ -1,1 +1,8 @@
+import Vue from "vue";
+import Dashboard from "@/views/Dashboard.vue";
 
+describe("Dashboard.vue", () => {
+  test("setup correctly", () => {
+    expect(true).toBe(true);
+  });
+});
