5132e64706fb0b875e210c7f1b9977a5d354113b
soumyat97 <37169644+soumyat97@users.noreply.github.com>
2020-10-27 10:55:54 +0530

    Add files via upload

---
+++ src/components/Footer.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="bottom">
+    <br />
+    <hr />
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+    <hr />
+    <br />
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+#bottom {
+  position: relative;
+  bottom: 0%;
+}
+</style>
