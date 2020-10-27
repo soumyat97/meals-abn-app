ca85cf6c7ba6688f1db90aad68cd8b7084d18829
soumyat97 <37169644+soumyat97@users.noreply.github.com>
2020-10-27 10:55:20 +0530

    Create Header.vue

---
+++ src/components/Header.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <br />
+    <header id="header">
+      <nav class="navbar navbar-inverse" role="banner">
+        <div class="container">
+          <div class="navbar-header">
+            <router-link to="/"
+              ><img
+                src="../assets/images/logo-small.png"
+                alt="logo"
+                width="100%"
+            /></router-link>
+          </div>
+        </div>
+        <!--/.container-->
+      </nav>
+      <!--/nav-->
+    </header>
+    <!--/header-->
+    <hr />
+  </div>
+</template>
+<script>
+export default {
+  name: "Header"
+};
+</script>
+<style scoped></style>
